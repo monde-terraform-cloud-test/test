ba642b3c0f8fee14f0169ffab2d958ae3b014403
s.monde <s.monde@edion.co.jp>
2022-12-12 19:42:56 +0900

    ec2

--- network.tf
+++ network.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "terraform_public_1a" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "192.168.2.0/25"
-  availability_zone = "${var.aws.region}"
+  availability_zone = "ap-northeast-1"
 
   tags = {
     Name = "terraform-public-1a"
@@ -35,7 +35,7 @@ data "aws_ssm_parameter" "amzn2_latest_ami" {
 resource "aws_instance" "terraform_ec2"{
   ami                         = data.aws_ssm_parameter.amzn2_latest_ami.value
   instance_type               = "t2.micro"
-  availability_zone           = "${var.aws.region}"
+  availability_zone           = "ap-northeast-1"
   vpc_security_group_ids      = "sg-0a71e946535eb00dc"
   subnet_id                   = aws_subnet.terraform_public_1a
   associate_public_ip_address = "true"
